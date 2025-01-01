3bc48e434d0e70aad4e05481d3665a94fbd4ade2
devendra reddy <devendranathreddy54@gmail.com>
2025-01-01 18:45:48 +0530

    objects, strings

--- objects/codeExample_4.js
+++ objects/codeExample_4.js
@@ -1,11 +1,11 @@
 let codes = {
   '+91': "India",
   '+61': "Australia",
-  "name":'Dev',
+  'name':'Dev',
   '+64': "New Zealand",
   '+1': "USA" 
-};
+}; 
 
 for (let code in codes) {
-  console.log(`${code} - ${codes[code]}`);
+console.log(`${code}-${codes[code]}`);
 }
